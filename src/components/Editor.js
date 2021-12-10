afb5c22a9070311f4dfd2432df956f84b2fe1c27
Yash <naravade.11809605@lpu.in>
2021-12-11 02:02:21 +0530

    Designing changes

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react'
 import ReactMarkdown from 'react-markdown';
 import "./Editor.css";
+import iconImg from './editor-icon.png';
 
 function Editor() {
 
@@ -14,8 +15,13 @@ function Editor() {
     <>
         <div className="main-heading">
         
-        <h2><img src="https://img.icons8.com/fluency/69/000000/markdown.png"/> React Markdown Editor</h2>
+        <h2><img src={iconImg} alt="icon" style={{width:'69px'}}/> GoGo Markdown Editor</h2>
         </div>
+
+        <div class="button">
+        <a href="#">Downlоad</a>
+    </div>
+
       <div className="container-fluid body">
         <div className="row">
           <div className="col-md-6 editor-window">
@@ -47,7 +53,7 @@ function Editor() {
         <div className="page-footer font-small blue">
         
         <div className="footer-copyright text-center py-3">
-            Made with <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24"><path d="M12 4.435c-1.989-5.399-12-4.597-12 3.568 0 4.068 3.06 9.481 12 14.997 8.94-5.516 12-10.929 12-14.997 0-8.118-10-8.999-12-3.568z"/></svg> by <a href="https://github.com/yashnaravade" style={{}} className='footer-anchor'> Yash Naravade</a>
+            Made with <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24"><path d="M12 4.435c-1.989-5.399-12-4.597-12 3.568 0 4.068 3.06 9.481 12 14.997 8.94-5.516 12-10.929 12-14.997 0-8.118-10-8.999-12-3.568z"/></svg> by <a href="https://github.com/yashnaravade"  className='footer-anchor'> Yash Naravade</a>
         </div>
       
     </div>
